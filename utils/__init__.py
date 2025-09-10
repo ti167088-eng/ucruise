63051288979c37a960bb9a8b812b1820fa9548df
ti167088-eng <ti167088@gmail.com>
2025-09-10 17:15:27 +0530

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utilities package for route assignment system
-"""
-from .logger_config import get_logger
-from .progress_tracker import get_progress_tracker
-
-__all__ = ['get_logger', 'get_progress_tracker']
